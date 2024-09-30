e12709255e42a35d7a4e2a5b1665b1d033c8402d
Prashant <101509405+Prashant501Tyagi@users.noreply.github.com>
2024-09-30 15:20:55 +0530

    changes in test case:

--- test/test.py
+++ test/test.py
@@ -5,11 +5,11 @@ from src.app import add,minus
 
 def test_add():
     assert add(1,2) == 3
-    assert add(1,2) == 5
     assert add(1,6) == 7
+    assert add(1,9) == 10
 
 def test_minus():
     assert minus(5,3)==2
-    assert minus(5,3)==6
+    assert minus(5,1)==4
     assert minus(10,3)==7
 
